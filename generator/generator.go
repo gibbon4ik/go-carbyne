5cebef03fad9566649d1fd50f582c0e2e8c7a0db
Igor Golubev <gibbon4ik@gmail.com>
2020-12-27 00:30:40 +0300

    enchance generator

--- generator/generator.go
+++ generator/generator.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -15,7 +17,7 @@ var (
 	delay      int
 	remoteip   string
 	localip    string
-	prefix     string
+	filename   string
 	remoteaddr *net.UDPAddr
 	localaddr  *net.UDPAddr
 	wg         sync.WaitGroup
@@ -30,7 +32,7 @@ func main() {
 	flag.IntVar(&delay, "d", 0, "Time in us between packets")
 	flag.StringVar(&remoteip, "r", "127.0.0.1:2023", "Send packets on host:port")
 	flag.StringVar(&localip, "l", "", "Send packets from host:port")
-	flag.StringVar(&prefix, "prefix", "test", "Metrics prefix")
+	flag.StringVar(&filename, "f", "", "File name with metrics")
 	flag.Parse()
 
 	if remoteip == "" {
@@ -49,9 +51,18 @@ func main() {
 		}
 	}
 
+	if filename != "" {
+		for i := 0; i < workers; i++ {
+			wg.Add(1)
+			go replay(&wg, filename)
+		}
+		wg.Wait()
+		os.Exit(0)
+	}
+
 	tail := flag.Args()
 	if len(tail) == 0 {
-		panic("usage: " + os.Args[0] + " [-options] key value [key value ...]")
+		panic("usage: " + os.Args[0] + " [-options] [key value ...]")
 	}
 	if len(tail)%2 != 0 {
 		panic("uneven arguments number")
@@ -78,16 +89,71 @@ func sender(wg *sync.WaitGroup) {
 	}
 	defer conn.Close()
 
-	for i := 0; i < count; i++ {
-		_, err := conn.Write(metrics)
-		if err != nil {
-			fmt.Println(err)
+	if delay > 0 {
+		i := count
+		for _ = range time.Tick(time.Microsecond * time.Duration(delay)) {
+			_, err := conn.Write(metrics)
+			if err != nil {
+				fmt.Println(err)
+			}
+			i--
+			if i == 0 {
+				break
+			}
 		}
-		if delay > 0 {
-			time.Sleep(time.Duration(delay) * time.Microsecond)
+	} else {
+		for i := 0; i < count; i++ {
+			_, err := conn.Write(metrics)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 	if delay < 10 {
 		time.Sleep(time.Second)
 	}
+}
+
+func replay(wg *sync.WaitGroup, filename string) {
+	defer wg.Done()
+
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	conn, err := net.DialUDP("udp", localaddr, remoteaddr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	sc := bufio.NewScanner(f)
+	buf := ""
+	buflen := 0
+	strl := 10
+	for sc.Scan() {
+		tm := strconv.FormatInt(time.Now().Unix(), 10)
+
+		line := sc.Text() // GET the line string
+		llen := len(line)
+		line = line[0:llen-strl] + tm + "\n"
+		llen++
+		if buflen+llen > 1470 {
+			_, err := conn.Write([]byte(buf))
+			if err != nil {
+				fmt.Println(err)
+			}
+			if delay > 0 {
+				time.Sleep(time.Duration(delay) * time.Microsecond)
+			}
+			buf = line
+			buflen = llen
+			continue
+		}
+		buf += line
+		buflen += llen
+	}
+
 }
